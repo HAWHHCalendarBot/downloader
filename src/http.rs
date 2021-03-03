5353692df0150244022601cf4726cd11190486d3
EdJoPaTo <git-commit-with-a-funny-long-email-address-everyone-hates-as-it-is-too-long@edjopato.de>
2021-03-03 09:43:38 +0100

    fix(http): omit url as error always contains url

--- src/http.rs
+++ src/http.rs
@@ -14,7 +14,7 @@ fn get_with_headers(agent: &Agent, url: &str) -> Request {
 pub fn get_text(agent: &Agent, url: &str) -> Result<String, String> {
     get_with_headers(agent, url)
         .call()
-        .map_err(|err| format!("failed to get {} {}", url, err))?
+        .map_err(|err| format!("failed to get {}", err))?
         .into_string()
         .map_err(|err| format!("failed to read string {} {}", url, err))
 }
@@ -24,7 +24,7 @@ pub fn get_haw_text(agent: &Agent, url: &str) -> Result<String, String> {
 
     get_with_headers(agent, url)
         .call()
-        .map_err(|err| format!("failed to get {} {}", url, err))?
+        .map_err(|err| format!("failed to get {}", err))?
         .into_reader()
         .read_to_end(&mut bytes)
         .map_err(|err| format!("failed to read bytes {} {}", url, err))?;
